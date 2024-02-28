566c46992bece96a8e66bc0e10d3115695e837b8
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-02-29 04:38:52 +0700

    Update downloader-ytmp4.js

--- router/downloader-ytmp4.js
+++ router/downloader-ytmp4.js
@@ -2,20 +2,35 @@ const express = require('express');
 const router = express.Router();
 const { youtubedl } = require('@bochilteam/scraper-sosmed');
 
+router.get('/', (req, res) => {
+    res.status(400).json({ error: 'Silakan masukkan ID YouTube setelah /downloader-ytmp4/' });
+});
+
 router.get('/:url', async (req, res) => {
     try {
         // Ekstrak URL YouTube dari parameter URL
         const videoUrl = req.params.url.split('/').pop();
+        
+        // Jika URL YouTube kosong, kirim pesan kesalahan
+        if (!videoUrl) {
+            throw new Error('ID YouTube tidak ditemukan');
+        }
+        
         const data = await youtubedl(`https://youtu.be/${videoUrl}`);
         
-        // Ambil video stream dari data
-        const videoStream = await data.video['720p'].stream();
+        // Periksa jika video dengan resolusi '720p' tersedia
+        if (!data.video['720p']) {
+            throw new Error('Video not available in 720p resolution');
+        }
+        
+        // Ambil URL video dari data
+        const videoUrl = data.video['720p'].url;
         
         // Set header untuk memberitahu browser bahwa ini adalah video
         res.setHeader('Content-Type', 'video/mp4');
         
-        // Alirkan video stream ke respons
-        videoStream.pipe(res);
+        // Redirect ke URL video
+        res.redirect(videoUrl);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }

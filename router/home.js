88e93714a46c0342927672dbf6a9ccdd07f88caa
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-03-01 18:42:04 +0700

    Update home.js

--- router/home.js
+++ router/home.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Middleware untuk menangani permintaan GET ke /donate
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/Test', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public/Dll', 'home.html'));
 });
 
 module.exports = router;

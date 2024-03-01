d71550a336fe3323151ed6a282d6eabf6a5ad34f
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-03-02 01:14:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const chalk = require('chalk'); // Using require to import Chalk
+import chalk from 'chalk'; // Using require to import Chalk
 
 const app = express();
 const port = process.env.PORT || 8080;

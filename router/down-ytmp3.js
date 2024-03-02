8696cd4df986e86132625ccbd4edeb5ec8786f8d
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-03-02 22:22:33 +0000

    ++ YTMP3 🔥

---
+++ router/down-ytmp3.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { youtubedl } = require('@bochilteam/scraper-sosmed');
+
+const router = express.Router();
+
+router.get('/:videoId?', async (req, res) => {
+    try {
+        const { videoId } = req.params;
+        
+        if (!videoId) {
+            return res.status(400).json({ error: 'URL tidak diisi. Silakan masukkan ID video YouTube. | Contoh : /ytmp3/id' });
+        }
+        
+        const videoUrl = `https://youtu.be/${videoId}`;
+        
+        const data = await youtubedl(videoUrl);
+        const { title } = data;
+        const url = await data.audio['128kbps'];
+        
+        res.json({ title, url });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+module.exports = router;

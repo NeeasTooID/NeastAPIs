f44ca4179d3423486be2f582373146273167175f
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-03-05 15:30:18 +0700

    Update anime-waifuv2.js

--- router/anime-waifuv2.js
+++ router/anime-waifuv2.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
+const axios = require('axios');
 
-router.get('/', (req, res) => {
-    // Lakukan HTTP request untuk mengunduh gambar dari URL
-    const imageUrl = "https://datanst.zanixon.xyz/random-waifuv2";
-    const requestSettings = {
-        url: imageUrl,
-        method: 'GET',
-        encoding: null
-    };
+router.get('/', async (req, res) => {
+    try {
+        // Lakukan HTTP request untuk mengunduh gambar dari URL
+        const imageUrl = "https://datanst.zanixon.xyz/random-waifuv2";
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
 
-    request(requestSettings, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // Mengirimkan gambar sebagai respons
-            res.set('Content-Type', 'image/jpeg'); // Set jenis konten sebagai gambar JPEG
-            res.send(body);
-        } else {
-            res.status(500).json({ error: 'Gagal mengambil gambar' });
-        }
-    });
+        // Mengirimkan gambar sebagai respons
+        res.set('Content-Type', 'image/jpeg'); // Set jenis konten sebagai gambar JPEG
+        res.send(Buffer.from(response.data, 'binary'));
+    } catch (error) {
+        console.error('Gagal mengambil gambar:', error.message);
+        res.status(500).json({ error: 'Gagal mengambil gambar' });
+    }
 });
+
+module.exports = router;

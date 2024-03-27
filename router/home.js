1c1f58729e19fbe4ca7a9eec824ea0e7f2c6afd9
CupMenolakMc <156963989+NeastooID@users.noreply.github.com>
2024-03-27 09:43:08 +0000

    ntr malem

--- router/home.js
+++ router/home.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Middleware untuk menangani permintaan GET ke /donate
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/Dll', 'mt.html'));
+    res.sendFile(path.join(__dirname, '../public/Dll', 'home.html'));
 });
 
 module.exports = router;
